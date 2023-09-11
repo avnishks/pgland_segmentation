bb96700b55c3b08b0e52680c2bea7ec5149b755d
Kathleen E Larson <kl021@azura.nmr.mgh.harvard.edu>
2023-09-11 13:17:08 -0400

    Adapted the unetxd framework from sean

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -4,197 +4,238 @@ import torch.nn.functional as F
 
 
 
-# Define the U-Net model
 class UNet3D(nn.Module):
     def __init__(self,
                  in_channels:int,
                  out_channels:int,
-                 n_features_start:int=24, # this could just be hard coded? depends if n=24 is valid
-                 n_blocks:int=4,
                  n_convs_per_block:int=2,
+                 block_config:tuple=(24, 48, 96, 192), #switch this back to my format later
                  activation_type:str='ReLU',
                  pooling_type:str='MaxPool3d',
-                 **kwargs):
+                 skip:bool=True,
+                 **kwargs
+    ):
         super(UNet3D, self).__init__()
-        
-        # Validate arguments
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.n_features_start = n_features_start
-        self.n_blocks = n_blocks
-        self.n_convs_per_block = n_convs_per_block
+
+        self.block_config = list(block_config)
+        self.n_blocks = len(block_config)
+        self.skip = skip
 
         self.activation_type = activation_type if callable(getattr(nn, activation_type)) \
             else Exception("Invalid activation_type (not an attribute of torch.nn")
         self.pooling_type = pooling_type if callable(getattr(nn, pooling_type)) \
             else Exception("Invalid pooling_type (not an attribute of torch.nn")
-        
-        self.blocks = nn.Sequential()
 
         
-        # Encoding blocks (down = conv + activation --> pooling)
-        n_input_features = in_channels
-        n_output_features = n_features_start
+        # Encoding blocks:
+        self.encoding = nn.Sequential()
+        encoding_config = [in_channels] + self.block_config
         
-        for b in range(0, n_blocks):
-            if b == n_blocks - 1:
-                block = _UNet3D_DownBlock(in_channels=n_input_features,
-                                          out_channels=n_output_features,
-                                          n_convs_per_block=n_convs_per_block,
-                                          activation_type=activation_type,
-                                          pooling_type=pooling_type,
-                                          pool=False,
-                )
-            else:
-                block = _UNet3D_DownBlock(in_channels=n_input_features,
-                                          out_channels=n_output_features,
-                                          n_convs_per_block=n_convs_per_block,
-                                          activation_type=activation_type,
-                                          pooling_type=pooling_type,
-                                          pool=True,
-                )
-            self.blocks.add_module('down%d' % (b+1), block)
-            n_input_features = n_output_features
-            n_output_features = n_output_features * 2
+        for b in range(0, len(encoding_config) - 1):
+            block = _UNet3D_Block(n_input_features=encoding_config[b],
+                                  n_output_features=encoding_config[b+1],
+                                  n_layers=n_convs_per_block,
+                                  activation_type=activation_type,
+                                  level=b,
+                                  norm=True,
+                                  drop=0,
+            )
+            self.encoding.add_module('ConvBlock%d' % (b+1), block)
+            pool = getattr(nn, pooling_type)(kernel_size=2,
+                                             stride=2,
+                                             return_indices=True
+            )
+            self.encoding.add_module('Pool%d' % (b+1), pool)
+                
+        #Decoding blocks:
+        self.decoding = nn.Sequential()
+        decoding_config = [out_channels] + self.block_config
         
+        for b in reversed(range(0, len(decoding_config) - 1)):
+            upsample = nn.MaxUnpool3d(kernel_size=2, stride=2)
+            self.decoding.add_module('Upsample%d' % (b+1), upsample)
+            block = _UNet3D_BlockTranspose(n_input_features=decoding_config[b+1],
+                                           n_output_features=decoding_config[b],
+                                           n_layers=n_convs_per_block,
+                                           activation_type=activation_type,
+                                           level=b,
+                                           norm=True,
+                                           drop=0,
+            )
+            self.decoding.add_module('ConvBlock%d' % (b+1), block)
             
-        # Decoding blocks (up = up_conv --> concatenate w/ skip --> conv + activation)
-        n_output_features = n_input_features // 2
+        
+        last = nn.Sequential(nn.ConvTranspose3d(decoding_config[0], 
+                                                out_channels=out_channels,
+                                                kernel_size=1,
+                                                stride=1),
+                             nn.Softmax(dim=1)
+        )
+        self.decoding.add_module('LastStep', nn.Softmax(dim=1))
+        #self.decoding.add_module('LastConv', last)
+        
 
-        for b in range(0, n_blocks):
-            if b == n_blocks - 1:
-                block = nn.Sequential(nn.Conv3d(n_input_features,
-                                                out_channels,
-                                                kernel_size=1),
-                                      nn.Softmax(dim=1))
-
-            else:
-                block = _UNet3D_UpBlock(in_channels=n_input_features,
-                                        out_channels=n_output_features,
-                                        n_convs_per_block=n_convs_per_block,
-                                        activation_type=activation_type
-                )
-            self.blocks.add_module('up%d' % (b+1), block)
-            n_input_features = n_output_features
-            n_output_features = n_output_features // 2
-
-            
 
     def forward(self, x):
         # Initialize intermediate steps
-        encoding = [None] * (self.n_blocks + 1)
-        decoding = [None] * (self.n_blocks + 1)
-        skips = [None] * (self.n_blocks - 1)
+        enc = [None] * (self.n_blocks) # encoding
+        dec = [None] * (self.n_blocks) # decoding
+        idx = [None] * (self.n_blocks) # maxpool indices
+        siz = [None] * (self.n_blocks) # maxunpool output size
+
+        printout = False
         
-        # Down
-        encoding[0] = x
-        for b in range(0, self.n_blocks):
-            #print("Down", b+1)
-            #print("input:", encoding[b].shape)
-            #print(self.blocks.__getattr__('down%d' % (b+1)))
-            if b != self.n_blocks - 1:
-                encoding[b+1], skips[b] = self.blocks.__getattr__('down%d' % (b+1))(encoding[b])
-            else:
-                encoding[b+1], _ = self.blocks.__getattr__('down%d' % (b+1))(encoding[b])
-            #print("output:", encoding[b+1].shape)
-            #print(" ")
-                       
-        # Up
-        decoding[0] = encoding[-1]
-        for b in range(0, self.n_blocks):
-            #print("Up", b+1)
-            #print("input:", decoding[b].shape)
-            #print(self.blocks.__getattr__('up%d' % (b+1)))
-            if b != self.n_blocks - 1:
-                skip_ind = self.n_blocks - b - 2
-                #print("skip:", skips[skip_ind].shape)
-                decoding[b+1] = self.blocks.__getattr__('up%d' % (b+1))(decoding[b], skips[skip_ind])
-            else:
-                decoding[b+1] = self.blocks.__getattr__('up%d' % (b+1))(decoding[b])
-            #print("output:", decoding[b+1].shape)
-            #print(" ")
-            
         #breakpoint()
-        return decoding[-1]
+        # Encoding
+        for b in range(0, self.n_blocks):
+            if printout:  print('---------')
+            if printout:  print('Encoding block %d' % (b+1))
+            if printout:  print('---------')
+            x = enc[b] = self.encoding.__getattr__('ConvBlock%d' % (b+1))(x)
+            if printout:  print(self.encoding.__getattr__('ConvBlock%d' % (b+1)))
+            siz[b] = x.shape
+            #if b != self.n_blocks - 1:
+            x, idx[b] =  self.encoding.__getattr__('Pool%d' % (b+1))(x)
+            if printout:  print(self.encoding.__getattr__('Pool%d' % (b+1)))
+            if printout:  print(' ')
                 
-
-
-# Downsampling (encoding) block
-class _UNet3D_DownBlock(nn.Module):
-    def __init__(self,
-                 in_channels:int,
-                 out_channels:int,
-                 n_convs_per_block:int,
-                 activation_type:str,
-                 pooling_type:str,
-                 pool=True,
-    ):
-        super(_UNet3D_DownBlock, self).__init__()
-        self.n_convs = n_convs_per_block
-        self.pool = pool
-        
-        # Define components
-        self.conv_block = nn.Sequential()
-        for n in range(0, n_convs_per_block):
-            if n == 0:
-                conv = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1)
-            else:
-                conv = nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1)
-
-            layer = nn.Sequential(conv,
-                                  nn.BatchNorm3d(out_channels),
-                                  getattr(nn, activation_type)(inplace=True)
-            )
-            self.conv_block.add_module('convlayer%d' % (n+1), layer)
-
-        if self.pool:
-            self.pooling = getattr(nn, pooling_type)(kernel_size=2, stride=2)
+        # Decoding
+        for b in reversed(range(0, self.n_blocks)):
+            if printout:  print('---------')
+            if printout:  print('Decoding block %d' % (b+1))
+            if printout:  print('---------')
+            #if b != self.n_blocks - 1:
+            x = self.decoding.__getattr__('Upsample%d' % (b+1))(x, idx[b], output_size=siz[b])
+            if printout:  print(self.decoding.__getattr__('Upsample%d' % (b+1)))
+            x = dec[b] = self.decoding.__getattr__('ConvBlock%d' % (b+1))(torch.cat([x, enc[b]], 1))
+            if printout:  print('w/ skip', self.decoding.__getattr__('ConvBlock%d' % (b+1)))
+            if printout:  print(' ')
             
+        x = self.decoding.LastStep(x)
+        return x
+
+            
+        
+class _UNet3D_Layer(nn.Module):
+    def __init__(self,
+                 n_input_features:int,
+                 n_output_features:int,
+                 activation_type:str,
+                 norm=True,
+                 drop=0,
+                 activ=True,
+                 **kwargs
+    ):
+        super(_UNet3D_Layer, self).__init__()
+
+        self.add_module('norm', nn.BatchNorm3d(n_input_features) if norm else nn.Identity())
+        self.add_module('activ', nn.ELU(inplace=False) if activation_type is not None else nn.Identity())
+        self.add_module('conv', nn.Conv3d(n_input_features, n_output_features, kernel_size=3, padding=1,
+                                          bias=False if norm else True))
+        self.add_module('drop', nn.Dropout3d(drop) if drop > 0 else nn.Identity())
 
     def forward(self, x):
-        for n in range(0, self.n_convs):
-            x = self.conv_block.__getattr__('convlayer%d' % (n+1))(x)
-
-        if self.pool:
-            return self.pooling(x), x
-        else:
-            return x, x
+        return self.drop(self.conv(self.activ(self.norm(x))))
 
 
-
-# Define the upsampling (decoding) block
-class _UNet3D_UpBlock(nn.Module):
+    
+class _UNet3D_LayerTranspose(nn.Module):
     def __init__(self,
-                 in_channels:int,
-                 out_channels:int,
-                 n_convs_per_block:int,
-                 activation_type:str
+                 n_input_features:int,
+                 n_output_features:int,
+                 activation_type:str,
+                 norm=True,
+                 drop=0,
+                 **kwargs
     ):
-        super(_UNet3D_UpBlock, self).__init__()
-        self.n_convs = n_convs_per_block
+        super(_UNet3D_LayerTranspose, self).__init__()
         
-        # Define components
-        self.up = nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.add_module('drop', nn.Dropout3d(drop) if drop > 0 else nn.Identity())
+        self.add_module('norm', nn.BatchNorm3d(n_input_features) if norm else nn.Identity())
+        self.add_module('activ', nn.ELU(inplace=False) if activation_type is not None else nn.Identity())
+        self.add_module('conv', nn.ConvTranspose3d(n_input_features, n_output_features, kernel_size=3, padding=1,
+                                          bias=False if norm else True))
+        
+    def forward(self, x):
+        return self.conv(self.activ(self.norm(self.drop(x))))
 
-        self.conv_block = nn.Sequential()
-        for n in range(0,n_convs_per_block):    
-            if n == 0:
-                conv = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1)
-            else:
-                conv = nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1)
 
-            layer = nn.Sequential(conv,
-                                  nn.BatchNorm3d(out_channels),
-                                  getattr(nn, activation_type)(inplace=True)
+
+
+class _UNet3D_Block(nn.ModuleDict):
+    def __init__(self,
+                 n_input_features:int,
+                 n_output_features:int,
+                 n_layers:int,
+                 activation_type:str,
+                 level:int,
+                 norm=True,
+                 drop=0,
+                 skip=False,
+                 **kwargs
+    ):
+        super(_UNet3D_Block, self).__init__()
+
+        layer = _UNet3D_Layer(n_input_features=n_input_features,
+                              n_output_features=n_output_features,
+                              activation_type=activation_type if level != 0 else None,
+                              norm=norm,
+                              drop=drop,
+        )
+        self.add_module('ConvLayer1', layer)
+
+        for i in range(1, n_layers):
+            growth = 1 + (skip and i == n_layers - 1)
+            layer = _UNet3D_Layer(n_input_features=n_output_features,
+                                  n_output_features=growth*n_output_features,
+                                  activation_type=activation_type,
+                                  norm=norm,
+                                  drop=drop,
             )
-            self.conv_block.add_module('convlayer%d' % (n+1), layer)
-        
+            self.add_module('ConvLayer%d' % (i + 1), layer)
 
-    def forward(self, x, skip):
-        x = self.up(x)
-        x = torch.cat([x, skip], dim=1)
-        
-        for n in range(0, self.n_convs):
-            x = self.conv_block.__getattr__('convlayer%d' % (n+1))(x)
             
+    def forward(self, x):
+        for name, layer in self.items():
+            x = layer(x)
         return x
+
+
+    
+class _UNet3D_BlockTranspose(nn.ModuleDict):
+    def __init__(self,
+                 n_input_features:int,
+                 n_output_features:int,
+                 activation_type:str,
+                 n_layers:int,
+                 level:int,
+                 norm=True,
+                 drop=0,
+                 skip=True,
+                 **kwargs
+    ):
+        super(_UNet3D_BlockTranspose, self).__init__()
+        for i in reversed(range(1,n_layers)):
+            growth = 1 + (skip and i == n_layers - 1)
+            layer = _UNet3D_LayerTranspose(n_input_features=growth*n_input_features,
+                                           n_output_features=n_input_features,
+                                           activation_type=activation_type,
+                                           norm=norm,
+                                           drop=drop,
+            )
+            self.add_module('ConvLayer%d' % (i + 1), layer)
+
+        layer = _UNet3D_LayerTranspose(n_input_features=n_input_features,
+                                       n_output_features=n_output_features,
+                                       activation_type=activation_type,
+                                       norm=norm,
+                                       drop=drop,
+        )
+        self.add_module('ConvLayer1', layer)
+
+
+    def forward(self, x):
+        for name, layer in self.items():
+            x = layer(x)
+        return x
+        
